30834c883987213a8c0fcf83550796d50d4b992d
Josiah Parry <josiah.parry@gmail.com>
2024-05-06 09:28:53 -0400

    updates draft duckdb notes

---
+++ duckdb-deep-dive/dev.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(dbplyr)
+library(duckdb)
+library(nycflights13)
+
+drv <- duckdb()
+con <- dbConnect(drv)
+
+# copy the df to duckdb
+copy_to(con, flights)
+copy_to(con, weather)
+
+# list the tables in the db
+
+dbListTables(con)
+
+
+# make a connection object to it
+flights <- tbl(con, "flights")
+weather <- tbl(con, "weather")
